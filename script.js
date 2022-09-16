7026edca08203f2af445f1fae257e771d8c32e7e
PechMV <Pechm607@gmail.com>
2022-09-15 22:59:50 -0500

    added prompt messages to js file

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+
+  var userInput = window.prompt("How long do you want your password to be?")
+  var passwordLength = parseInt(userInput)
+
+    if (isNaN(passwordLength)) {
+      window.alert("That's not a number. Please try again.")
+      return
+    }
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Password length must be between 8 and 128 characters")
+      return
+    }
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
