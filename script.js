059c07af51694e0e461a57ac54f00175643665f6
PechMV <Pechm607@gmail.com>
2022-09-16 19:04:26 -0500

    added lowercase,uppercase, number and special character prompts

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  var userInput = window.prompt("How long do you want your password to be?")
+  var userInput = window.prompt("How long do you want the password to be?")
   var passwordLength = parseInt(userInput)
 
     if (isNaN(passwordLength)) {
@@ -16,6 +16,11 @@ function generatePassword() {
       window.alert("Password length must be between 8 and 128 characters")
       return
     }
+
+    var userWantLowercase = window.confirm("Would you like to inlude lowercase letters in the password?");
+    var userWantUppercase = window.confirm("Would you like to include uppercase letters in the password?");
+    var userWantNumbers = window.confirm("Would you like to include numbers in the password?");
+    var userWantSpecial = window.confirm("Would you like to include special characters in the password?");
 }
 
 // Write password to the #password input
